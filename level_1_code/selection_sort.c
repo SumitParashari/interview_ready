837c1f007a900be569a77529316316ffb1c3c2a6
SumitParashari <sumitparashari.91@gmail.com>
2021-06-23 22:59:07 +0530

    selection sort done

--- level_1_code/selection_sort.c
+++ level_1_code/selection_sort.c
@@ -1,30 +1,50 @@
-// For ascending order slection sort:
+// For ascending order slection sort: O(n*n) - time complexity
 // Find the minimum element in unsorted array and swap it with element at the begining.
 
 
 #include <stdio.h>
 
-void selection_sort(int **array, int size)
+#define MAX_SIZE 10
+int time_complexity =0;
+
+void selection_sort(int data[], int size)
 {
-     for(i; i< size; i++)
+     int i,j, min_index,tmp;
+     for(i=0; i< size-1; i++)
      {
-	     for(j; j<)
+	     min_index = i;
+	     time_complexity++;
+	     for(j=i+1; j<size; j++)
+	     {
+		     time_complexity++;
+		     if(data[j]<data[min_index])
+		         min_index = j;
+	     }
+	     if(min_index != i)
+	     {
+		     tmp = data[i];
+		     data[i] = data[min_index];
+		     data[min_index] = tmp;
+	     }
      }
+
+
+     for(i=0; i < size; i++)
+     {
+            printf("%d ", data[i]);
+     }
+     printf("\n time_complexity %d\n",time_complexity);
 }
 
 int main()
 {
-	int i,data[10];
+	int i,data[MAX_SIZE];
         int size = sizeof(data)/sizeof(data[0]);
-	
+        
+        printf("Enter %d number:\n", MAX_SIZE);	
 	for(i=0; i < size; i++)
 	{
 	    scanf("%d", &data[i]);
 	}
-	 
-	for(i=0; i < size; i++)
-        {
-            printf("%d ", data[i]);
-        }
-	printf("\n");
+	selection_sort(data, size); 
 }
