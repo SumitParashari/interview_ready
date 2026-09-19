401aa14fc08b4a628d70876b2894d4a6a52a90ec
SumitParashari <31201675+SumitParashari@users.noreply.github.com>
2026-09-19 22:56:43 +0530

    Added add_to_head LL function.

--- linked_list/add_del_update_print.c
+++ linked_list/add_del_update_print.c
@@ -1,1 +1,27 @@
 // Library to add, del, update, print
+typedef struct linked_list NODE;
+struct linked_list {
+    int data;
+    NODE *next;
+};
+
+int add_to_head(NODE *head, int data) {
+  NODE *new_node = (NODE *)malloc(sizeof(NODE));
+  if(new_node == NULL) {
+    printf("Mem LOW\n");
+    return -1;
+  }
+  
+  new_node->data = data;
+
+  // Add to empty list.
+  if(head == NULL) {
+    new_node->next = NULL;
+    head = new_node;
+  } else {
+    new_node->next = head;
+    head = new_node;
+  }
+
+  return 0;
+}
