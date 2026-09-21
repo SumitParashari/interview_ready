608ac5a42b1a84aeb4a9ee95b4061dae06a7f8c9
SumitParashari <31201675+SumitParashari@users.noreply.github.com>
2026-09-21 20:49:34 +0530

    Create main.c

---
+++ linked_list/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // This is preprocessor directives, these are of 4 types 
+                   // 1- File inclusion, 2- Macro Substitution, 3- Conditional Compilation
+                   // 4- Compiler Control(like #pragma and #error)
+#include "add_del_update_print.h"
+
+int main(int argc, char **argv) {
+  NODE *head = NULL;
+
+  addNode();
+}
